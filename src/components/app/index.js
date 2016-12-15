3684f4218c83616ede8b829279fea7d39c759d7d
Ian Deans <ideans715@gmail.com>
2016-12-15 11:09:02 -0800

    Remove hand value from the render of hand and remove duplicate handValue function from hand component.

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -57,11 +57,14 @@ export default class App extends Component {
       role: 'dealer'
     }
     dealer.hand.value = 0
+    dealer.hand.bet = 0
 
     const ai_1 = { name: aiNames[0], bank: 100, hand: [], role: 'ai' }
     ai_1.hand.value = 0
+    ai_1.hand.bet = 0
     const ai_2 = { name: aiNames[1], bank: 100, hand: [], role: 'ai' }
     ai_2.hand.value = 0
+    ai_2.hand.bet = 0
 
     const player = {
       name: playerName,
@@ -70,6 +73,7 @@ export default class App extends Component {
       role: 'player'
     }
     player.hand.value = 0
+    player.hand.bet = 0
     this.setState({ dealer, ai_1, ai_2, player, round })
   }
 
@@ -198,7 +202,7 @@ export default class App extends Component {
         <div className="app">
           <GameTable ai_1={ai_1} ai_2={ai_2} dealer={dealer} deck={deck} player={player} round={round} />
           <PlayerUI
-            testDeal={this.testDeal} 
+            testDeal={this.testDeal}
             reset={this.newRound}
             showCard={this.showDealerCard}
             hitItPlayer={this.hitItPlayer}
