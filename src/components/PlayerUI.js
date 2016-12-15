f79dd1e715c8dd65b6bcfc20ac9b02289b767c71
Debrena McEwen <debrenamcewen@gmail.com>
2016-12-14 17:41:00 -0800

    Alter hit function to take an argument specifying which hand the card is going to. Add button to UI to test hit function on dealer.

--- src/components/PlayerUI.js
+++ src/components/PlayerUI.js
@@ -14,13 +14,22 @@ const PlayerUI = ({ hitItPlayer, playerBank, reset, showCard, testDeal }) => {
     showCard()
   }
 
+  const handleHit = () => {
+    hitItPlayer("player")
+  }
+
+  const handleHitDealer = () => {
+    hitItPlayer("dealer")
+  }
+
   return (
     <div className="player-ui">
       <span className="player-bank"><h3>{playerBank}</h3></span>
       <button onClick={ handleDeal } >Deal</button>
       <button onClick={ handleReset } >RESET</button>
       <button onClick={ handleShowCard } >show dealer</button>
-      <button onClick={ hitItPlayer } >Hit</button>
+      <button onClick={ handleHit } >Hit</button>
+      <button onClick={ handleHitDealer }>Hit Dealer</button>
     </div>
   )
 }
