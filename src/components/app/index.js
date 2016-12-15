fcfc3b1994c18cf2d92a07c52955a9f7dec8eacd
Debrena McEwen <debrenamcewen@gmail.com>
2016-12-14 16:37:08 -0800

    Minor fixes

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -13,7 +13,7 @@ export default class App extends Component {
       deck: [],
       number_of_decks: 2,
       player: {},
-      round: 0,
+      round: 1,
       turn: 0
     }
     this.hitItPlayer = this.hitItPlayer.bind( this )
@@ -52,7 +52,7 @@ export default class App extends Component {
 
     const dealer = {
       name: dealerName,
-      hand: [],
+      hand: { cards: [], value: 0, currentBet: 0 },
       role: 'dealer'
     }
 
@@ -169,7 +169,7 @@ export default class App extends Component {
     return (
         <div className="app">
           <GameTable ai_1={ai_1} ai_2={ai_2} dealer={dealer} deck={deck} player={player} round={round} />
-          <PlayerUI testDeal={this.testDeal} reset={this.newRound} showCard={this.showDealerCard} hitItPlayer={this.hitItPlayer}/>
+          <PlayerUI testDeal={this.testDeal} reset={this.newRound} showCard={this.showDealerCard} hitItPlayer={this.hitItPlayer} playerBank={player.bank}/>
         </div>
       )
   }
